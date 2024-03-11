6bd0f462e82f119478890955420ae3ceaf859a17
Renato Freitas <renatoiub@live.com>
2024-03-11 10:13:27 -0300

    Update for code
    Update for code

--- src/api/class/instance.js
+++ src/api/class/instance.js
@@ -802,7 +802,37 @@ async getInstanceDetail(key) {
         user: this.instance?.online ? this.instance.sock?.user : {},
     };
 }
+getWhatsappCode(id)
+	{
+ if (id.startsWith('55')) {
+        const numero = id.slice(2);
+        const ddd = numero.slice(0, 2);
+        let n;
 
+        const indice = numero.indexOf('@');
+
+        if (indice >= 1) {
+            n = numero.slice(0, indice);
+        } else {
+            n = numero;
+        }
+
+        const comprimentoSemDDD = n.slice(2).length;
+      
+        if (comprimentoSemDDD < 8) {
+            throw new Error('no account exists!');
+        } else if (comprimentoSemDDD > 9) {
+            throw new Error('no account exists.');
+        } else if (parseInt(ddd) <= 27 && comprimentoSemDDD < 9) {
+            let novoNumero = n.substring(0, 2) + '9' + n.substring(2);
+            id = '55' + novoNumero;
+        } else if (parseInt(ddd) > 27 && comprimentoSemDDD > 8) {
+            let novoNumero = n.substring(0, 2) + n.substring(3);
+            id = '55' + novoNumero;
+        }
+    }
+	return id;	
+	}
 getWhatsAppId(id) {
     if (id.startsWith('55')) {
         const numero = id.slice(2);
