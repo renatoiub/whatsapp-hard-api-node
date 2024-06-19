f8ae39650f39264616dea9a17dea26a4acc9754e
Renato Freitas <renatoiub@live.com>
2024-06-19 15:57:00 -0300

    Update index.js

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -4,14 +4,14 @@ const instanceRoutes = require('./instance.route');
 const messageRoutes = require('./message.route');
 const miscRoutes = require('./misc.route');
 const groupRoutes = require('./group.route');
-const managerRoutes = require('./manager.route');
+//const managerRoutes = require('./manager.route');
 
 router.get('/status', (req, res) => res.send('OK'));
 router.use('/instance', instanceRoutes);
 router.use('/message', messageRoutes);
 router.use('/group', groupRoutes);
 router.use('/misc', miscRoutes);
-router.get('/', (req, res) => res.redirect('/manager/login'));
-router.use('/manager', managerRoutes); // Adiciona as rotas de gerenciamento aqui
+//router.get('/', (req, res) => res.redirect('/manager/login'));
+//router.use('/manager', managerRoutes); // Adiciona as rotas de gerenciamento aqui
 
 module.exports = router;
